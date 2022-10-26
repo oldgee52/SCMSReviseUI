d47b152d9b52f35815af68834d45029b1528e154
Po-Hao KO <oldgee52@gmail.com>
2022-10-26 16:56:03 +0800

    finished draft of sidebar

---
+++ app.js
@@ -0,0 +1,7 @@
+const menuButton = document.querySelector("#menu-btn");
+const sidebar = document.querySelector("#sidebar");
+const container = document.querySelector(".my-container");
+menuButton.addEventListener("click", () => {
+  sidebar.classList.toggle("active-nav");
+  container.classList.toggle("active-cont");
+});
