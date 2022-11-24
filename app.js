e0b9394d97a5f88f1adbc28d83ab8a2cb4828869
Po-Hao KO <oldgee52@gmail.com>
2022-11-24 18:15:24 +0800

    finished sort table function

--- app.js
+++ app.js
@@ -54,15 +54,109 @@ const toggleSortTable = document.querySelectorAll(".sortable")
 
 toggleSortTable.forEach(sortTable => {
   sortTable.addEventListener("click", () => {
-    const asc = sortTable.classList.contains("asc")
     const desc = sortTable.classList.contains("desc")
     toggleSortTable.forEach(sortTable => {
       sortTable.classList.remove("asc", "desc")
     })
-    if (desc || !asc && !desc) {
+    if (desc) {
       sortTable.classList.add("asc")
     } else {
       sortTable.classList.add("desc")
     }
   })
 })
+
+
+const userData = [{
+  system: "TWM",
+  subID: 646456,
+  number: "0909123321",
+  name: "小小王",
+  createDate: "2018-12-30",
+  numberStatus: "正常使用中",
+  contract: "Y"
+},
+{
+  system: "070",
+  subID: 1232,
+  number: "07012325687",
+  name: "小小王",
+  createDate: "2019-12-30",
+  numberStatus: "正常使用中",
+  contract: ""
+},
+{
+  system: "INPPS",
+  subID: 645645,
+  number: "0909123548",
+  name: "小小王",
+  createDate: "2019-11-30",
+  numberStatus: "失效",
+  contract: ""
+}
+]
+const userPhoneInfo = document.getElementById("userPhoneInfo")
+
+const createRow = (obj, i) => {
+  const row = document.createElement("tr");
+  const objKeys = Object.keys(obj);
+  objKeys.map((key) => {
+    const cell = document.createElement("td");
+    cell.setAttribute("data-attr", key);
+    cell.innerHTML = obj[key];
+    row.appendChild(cell);
+  });
+  const indexRow = document.createElement("td");
+  const indexNode = document.createTextNode(i);
+  indexRow.appendChild(indexNode);
+  row.insertBefore(indexRow, row.children[0]);
+  return row;
+};
+
+const getTableContent = (data) => {
+  data.map((obj, i) => {
+    const row = createRow(obj, i + 1);
+    userPhoneInfo.appendChild(row);
+  });
+};
+
+const sortData = (data, param, direction = "asc") => {
+  userPhoneInfo.innerHTML = '';
+  const sortedData =
+    direction == "asc"
+      ? [...data].sort(function (a, b) {
+        if (a[param] < b[param]) {
+          return -1;
+        }
+        if (a[param] > b[param]) {
+          return 1;
+        }
+        return 0;
+      })
+      : [...data].sort(function (a, b) {
+        if (b[param] < a[param]) {
+          return -1;
+        }
+        if (b[param] > a[param]) {
+          return 1;
+        }
+        return 0;
+      });
+
+  getTableContent(sortedData);
+};
+
+window.addEventListener("load", () => {
+  getTableContent(userData);
+  [...toggleSortTable].map((button) => {
+    button.addEventListener("click", (e) => {
+      if (e.target.getAttribute("data-dir") == "desc") {
+        sortData(userData, e.target.id, "desc");
+        e.target.setAttribute("data-dir", "asc");
+      } else {
+        sortData(userData, e.target.id, "asc");
+        e.target.setAttribute("data-dir", "desc");
+      }
+    });
+  });
+});
